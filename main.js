1b5edb296ecc0da9626a11ddcc65efed9a30b572
Nick <240923518+nick-an11@users.noreply.github.com>
2025-11-21 16:49:12 +0100

    minor changes to the calculator styling. add animation effects using javascript for the main page

--- main.js
+++ main.js
@@ -3,5 +3,20 @@
     if (yearEl) {
         yearEl.textContent = new Date().getFullYear();
     }
+
+    const projectsCta = document.querySelector('[data-cta="projects"]');
+    if (projectsCta) {
+        const triggerAnimation = () => {
+            projectsCta.classList.remove('is-animating');
+            // Force reflow so animation can restart if button is clicked quickly
+            void projectsCta.offsetWidth;
+            projectsCta.classList.add('is-animating');
+        };
+
+        projectsCta.addEventListener('click', triggerAnimation);
+        projectsCta.addEventListener('animationend', () => {
+            projectsCta.classList.remove('is-animating');
+        });
+    }
 })();
 
